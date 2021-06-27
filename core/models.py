ace56ea04a28fde4815c5badb5dc6638dea28a59
Weslley Oliveira <weslleyoliveira258@gmail.com>
2021-06-27 19:22:32 -0300

    fix: fixing verbose_name of models

--- core/models.py
+++ core/models.py
@@ -9,17 +9,29 @@ class Base(models.Model):
 
 class Client(Base):
     name = models.CharField('Nome', max_length=255)
-    address_id = models.ForeignKey('Address', verbose_name='client_address', on_delete=models.CASCADE) 
+    address_id = models.ForeignKey('Address', on_delete=models.CASCADE) 
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
 
 
 class User(Base):
     name = models.CharField('Nome', max_length=255)
     birthdate = models.DateField('Data de Nascimento')
     cpf = models.CharField('CPF', max_length=15, unique=True)
-    client_id = models.ForeignKey('Client', verbose_name='user_client', on_delete=models.CASCADE)
-    address_id = models.ForeignKey('Address', verbose_name='user_address', on_delete=models.CASCADE)
+    client_id = models.ForeignKey('Client', on_delete=models.CASCADE)
+    address_id = models.ForeignKey('Address', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
 
 
 class Address(Base):
-    user_id = models.ForeignKey('User', verbose_name='address_user', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
     is_main = models.BooleanField('Endereço Principal?')
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
